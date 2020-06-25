c03547943424257d26126b15162617e6fcf9ec8a
diego murillo <diego.spd@gmail.com>
2020-06-25 10:16:19 -0500

    reduce bowling pins

--- src/katas/bowling/core.clj
+++ src/katas/bowling/core.clj
@@ -10,29 +10,37 @@
   [rolls]
   (loop [frames             []
          [first-roll
-          secon-roll
+          second-roll
           & remaining-rolls
           :as all-rolls]    rolls]
     (cond
       (roll/strike? first-roll)           (recur (conj frames (->Frame "X" first-roll 0 10))
                                                  (rest all-rolls))
-      (roll/spare? first-roll secon-roll) (recur (conj frames (->Frame "/" first-roll secon-roll 10))
+      (roll/spare? first-roll second-roll) (recur (conj frames (->Frame "/" first-roll second-roll 10))
                                                  remaining-rolls)
-      :else                               (recur (conj frames (->Frame nil first-roll secon-roll (+ first-roll secon-roll)))
+      :else                               (recur (conj frames (->Frame nil first-roll second-roll (+ first-roll second-roll)))
                                                  remaining-rolls))))
 
 (defn- reduce-frames-fn
   [first-frame second-frame]
-  (case (:type first-frame)
+  (case (if (= "acc" (:type first-frame))
+          (:type second-frame)
+          (:type first-frame))
     "X"
-    (->Frame "ACC" (:type second-frame) (+ 10 (:first-roll second-frame) (:second-roll second-frame)))
+    (->Frame "acc" (:first-roll second-frame) (:second-roll second-frame) (+ (:score first-frame) 10 (:first-roll second-frame) (:second-roll second-frame)))
 
     "/"
-    (+ 10 (* (:first-roll second-frame) (:second-roll second-frame)))
+    (->Frame "acc" (:first-roll second-frame) (:second-roll second-frame) (+ (:score first-frame)
+                                                                             10
+                                                                             (* 2 (:first-roll second-frame))
+                                                                             (:second-roll second-frame)))
+
 
     :else
+    (->Frame "acc" (:first-roll second-frame) (:second-roll second-frame) (+ (:score first-frame) (:first-roll second-frame) (:second-roll second-frame)))
     (+ (:first-roll first-frame) (:second-roll first-frame))))
 
 (defn total-score
   [rolls]
-  (reduce reduce-frames-fn rolls))
+  (let [acc (->Frame "acc" 0 0 0)]
+    (reduce reduce-frames-fn acc (map rolls->frames rolls))))
